76154f911079e1d01c37431e3ec02a759e45935c
Milan Sampath <codewmilan@gmail.com>
2025-01-12 18:12:46 +0530

    logic added

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ const inputSlider = document.querySelector("[slider-data]");
 const lengthDisplay = document.querySelector("[length-data]");
 const passwordDisplay = document.querySelector("[data-passwordDisplay]");
 const copyBtn = document.querySelector("[data-copy]");
-const copyMsg = document.querySelector("[Data-CopyMsg]");
+const copyMsg = document.querySelector("[data-CopyMsg]");
 const uppercaseCheck = document.querySelector("#uppercase");
 const lowercaseCheck = document.querySelector("#lowercase");
 const numbersCheck = document.querySelector("#numbers");
@@ -32,26 +32,60 @@ function handleSlider() {
  function getRandomInteger(min,max){
     return Math.floor(Math.random *(max-min)+ min ); 
  }
-function getRandomUppercase(){
+ function generateRandomNumber(){
+    return getRandomInteger(0,9)
+ }
+function generateUppercase(){
     return String.fromCharCode(getRandomInteger(65,91));
 }
-function getRandomLowercase(){
+function generateLowercase(){
     return String.fromCharCode(getRandomInteger(97,123));
 }
-function getRandomNumber(){
-    return getRandomInteger(0,9);
 
-}
-function getRandomSymbol(){
+function generateSymbol(){
     const randNum = getRandomInteger(0,symbols.length);
     return symbols.charAt(randNum);
 
     
 }
-function calcStrength(){
+function calcStrength(){ 
+   let hasUpper = false;
+   let hasLower = false;
+   let hasNum = false;
+   let hasSym = false;
+   if(uppercaseCheck.checked) hasUpper = true;
+   if(lowercaseCheck.checked) hasLower = true;
+   if(numberCheck.checked) hasNum = true;
+   if(symbolCheck.checked) hasSym = true;
    
+   if(hasUpper && hasLower && hasNum && hasSym && passwordLength>=9){
+    SetIndicator('#0f0');
+   }else if(
+    (hasLower || hasUpper) &&
+    (hasNum || hasSym) &&
+    passwordLength>=6
+   ){
+    SetIndicator("#ff0");
+   } else{
+    SetIndicator("#f00");
+   }
 }
-function copyContent{
-    navigator.clipboard.writeText(document.getElementById(""));
+async function copyContent(){
+    try{
+        await navigator.clipboard.writeText(passwordDisplay.value);
+        copyMsg.innerText = "copied"
+       
+    }
+    catch(e){
+        copyMsg.innerText ="failed"
+
+
+    }
+    //to make copy appear and dissapear
+    copyMsg.classList.remove("hidden");
+    setTimeout(() => {
+    
+    copyMsg.classList.add("hidden");    
+    }, 3000);
 
 }
